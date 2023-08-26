c7da9cc2ae089d7b3b7e19a25cab8b1cacf76c91
AndresDParra <143150603+AndresDParra@users.noreply.github.com>
2023-08-25 20:53:15 -0500

    added an elif in the moveset

--- delibird_moveset.py
+++ delibird_moveset.py
@@ -9,8 +9,11 @@ else:
     print("but it failed")
 
 Present_accuracy = random.uniform(0.0, 1.0)
+present_chance_to_heal = random.uniform(1.0,2.0)
 
 if Present_accuracy >= 0.3625:
     print("good luck")
+elif present_chance_to_heal >= (1.5) :
+    print("healed the opponent")
 else:
     print("but it failed")
